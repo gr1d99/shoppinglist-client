6bceccd270e088cb01900a446e672a925026dcaa
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-02-12 10:35:04 +0300

    [Feature]Add app default values file

---
+++ src/components/Defaults.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'http://localhost:5000';
+export const API_PREFIX = '/api/v1.0';
+export const LIST_LIMIT = 12; //for shopping lists.
+export const ITEM_LIST_LIMIT = 5; // for shopping items.
+export const SEARCH_LIMIT = 12; // for shopping items.
+export const PAGE = 1; // default start page.
