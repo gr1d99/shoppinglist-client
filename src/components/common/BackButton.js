af0a264a576e3e722da5c02e75962fb8c6e9138d
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-02-13 16:47:57 +0300

    [Feature]Rework back button functionality

--- src/components/common/BackButton.js
+++ src/components/common/BackButton.js
@@ -2,10 +2,28 @@ import React from 'react';
 
 export const backButton = WrappedClass => {
     return  class ShowBackButton extends React.Component {
+        goBack = e => {
+            switch (this.props.match.path) {
+                case "/shoppinglists/:id":
+                case "/shoppinglists/search":
+                    return this.props.history.push('/shoppinglists');
+
+                case "/shoppinglists/:id/items":
+                case "/shoppinglists/:id/items/:id/edit":
+                case "/shoppinglists/:id/items/create":
+                case "/shoppinglists/:id/edit":
+                    const id = this.props.match.url.match('[0-9]+')[0];
+                    return this.props.history.push(`/shoppinglists/${id}`);
+
+                default:
+                    return this.props.history.push('/')
+
+            }
+        }
         render() {
             return (
                 <div className="col-md-12">
-                    <button className="btn btn-default btn-xs pull-left" onClick={this.props.history.goBack}><span className="glyphicon glyphicon-arrow-left"></span> Go back</button>
+                    <button className="btn btn-default btn-xs pull-left" onClick={this.goBack}><span className="glyphicon glyphicon-arrow-left"></span> Go back</button>
                     <WrappedClass {...this.props}/>
                 </div>
             )
